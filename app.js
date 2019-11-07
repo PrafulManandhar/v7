aa567bbcda705ef692ad295d3b7fae87c8f00ff0
sushant suwal <sushantdsuwal@gmail.com>
2019-11-07 17:22:20 +0545

    create user

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
+const Event = require('./models/event');
+const User  = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        type User {
+          _id: ID!
+          email: String!
+          password: String!
+        }
+
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        input UserInput {
+          email: String!
+          password: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+        
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createUser: args =>{
+       return bcryptjs.hash(args.userInput.password, 12)
+        .then(hashPassword =>{
+          const user = new User({
+            email: args.userInput.email,
+            password: hashPassword
+          })
+          return user.save();
+        })
+        .then(result =>{
+          console.log(result)
+          return { ...result._doc,_id: result.id};
+        }).catch(err => {throw err});
+      }
+    },
+    graphiql: true
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+  process.env.MONGO_PASSWORD
+  }@events-booking-b34s2.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
